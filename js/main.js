c44662ecd13c9600e00cb9db4c2cdc5b4eeb75f8
Fred Carlin <thekreeperr@gmail.com>
2019-06-20 23:39:25 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,6 @@
-const github = document.getElementById('github');
-const linkedin = document.getElementById('linkedin');
 const fadeCards = document.querySelectorAll('.fade-card');
 
-function addPushRight() {
-    linkedin.classList.add("push-right");
-};
-
-function removePushRight() {
-    linkedin.classList.remove("push-right");
-};
-
-function addPushLeft() {
-    github.classList.add("push-left");
-};
-
-function removePushLeft() {
-    github.classList.remove("push-left");
-};
-
-function debounce(func, wait = 10, immediate = true) {
-    let timeout;
-    return function() {
-        let context = this, args = arguments;
-        let later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        let callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
-};
-
-function checkFade(e) {
+function checkFade() {
     fadeCards.forEach(fadeCard => {
         const bounding = fadeCard.getBoundingClientRect();
         const halfHeight = fadeCard.getBoundingClientRect().height / 2;
@@ -45,9 +12,10 @@ function checkFade(e) {
     });
 }
 
-github.addEventListener('mouseenter', addPushRight);
-github.addEventListener('mouseleave', removePushRight);
-linkedin.addEventListener('mouseenter', addPushLeft);
-linkedin.addEventListener('mouseleave', removePushLeft);
-window.addEventListener('DOMContentLoaded', debounce(checkFade));
-window.addEventListener('scroll', debounce(checkFade));
+window.addEventListener('DOMContentLoaded', checkFade);
+window.addEventListener('scroll', checkFade);
+
+$('.js-push').hover(function() {
+    $(this).next('.js-push').toggleClass('push-right');
+    $(this).prev('.js-push').toggleClass('push-left');
+});
